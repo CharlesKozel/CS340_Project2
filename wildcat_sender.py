cc3f04c9f0eb9f8ffe4a8662f05e92ce47a28706
Damian Vincent Dobrowolski <>
2025-10-26 14:30:01 -0500

    adds packet wrapping w/ seq num & checksum for new_packet()

--- wildcat_sender.py
+++ wildcat_sender.py
@@ -1,5 +1,7 @@
 import common
 import threading
+import struct
+import zlib
 
 class wildcat_sender(threading.Thread):
     def __init__(self, allowed_loss, window_size, my_tunnel, my_logger):
@@ -10,12 +12,32 @@ class wildcat_sender(threading.Thread):
         self.my_logger = my_logger
         self.die = False
         # add as needed
+        # seq num counter, start at 0
+        self.next_seq = 0
     
     def new_packet(self, packet_byte_array):
         ''' invoked when user sends a payload
         (Send with self.my_tunnel.magic_send(packet)) '''
+        # build MSG: 2B seq (uint16), payload, 2B checksum (CRC32 & OxFFFF)
+        # take curr seq
+        seq = self.next_seq & 0xFFFF
+        # pack seq num as 2Bs (big-endian)
+        seq_bytes = struct.pack("!H", seq)
+        # ensure payload is bytes
+        payload_bytes = bytes(packet_byte_array)
+        # body used for checksum = seq + payload
+        body = seq_bytes + payload_bytes
+        # compute checksum & keep lower 16 bits
+        checksum = zlib.crc32(body) & 0xFFFF
+        ck_bytes = struct.pack("!H", checksum)
+        # final MSG = seq(2) + payload + checksum(2)
+        msg = body + ck_bytes
+
         print(f"sending : {packet_byte_array}")
+        # send thru magic tunnel
         self.my_tunnel.magic_send(packet_byte_array)
+        # adv seq num (wrap at 2^16)
+        self.next_seq = (self.next_seq + 1) & 0xFFFF
 
     def receive(self, packet_byte_array):
         ''' invoked when an ACK arrives '''
