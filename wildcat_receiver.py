193d246730735635eb051c0d59f4adacb238b014
Charles Kozel <shadowck5000@gmail.com>
2025-10-26 21:23:29 -0500

    WIP reuse byte array manipulation across sender/receiver.  Receiver process messages and advances window, assumes 0% loss allowed

--- wildcat_receiver.py
+++ wildcat_receiver.py
@@ -1,44 +1,74 @@
+import math
+
 import common
 import threading
 import struct
 import zlib
+
+from wildcat_sender import get_seq_num, get_ck_sum, does_checksum_match, get_payload
+
 
 class wildcat_receiver(threading.Thread):
     def __init__(self, allowed_loss, window_size, my_tunnel, my_logger):
         super(wildcat_receiver, self).__init__()
         self.allowed_loss = allowed_loss
         self.window_size = window_size
+        # assume sequence starts at 0
+        self.rcv_wnd_seq_num = 0
+        self.received_window = {}
+
         self.my_tunnel = my_tunnel
         self.my_logger = my_logger
         self.die = False
         # add as needed
 
     def receive(self, packet_byte_array):
-        ''' invoked when a MSG arrives
-        Drop packets outside the receiver window '''
         print(f"received : {packet_byte_array}")
 
-        # extract seq num & ck sum
-        seq_num = self.get_seq_num(packet_byte_array)
-        ck_sum = self.get_ck_sum(packet_byte_array)
+        if not does_checksum_match(packet_byte_array):
+            return # drop corrupted packets
 
-        # compute checksum over seq + payload (excl. trailing cksum)
-        calc_ck = zlib.crc32(packet_byte_array[0:-2]) & 0xFFFF
+        seq_num = get_seq_num(packet_byte_array)
+        if self.is_outside_window(seq_num):
+            return # drop packet if outside window
 
-        # drop corrupted pkts
-        if calc_ck != ck_sum:
-            return
-        
-        # extract payload (btw seq & ck)
-        payload = packet_byte_array[2:-2]
+        payload = get_payload(packet_byte_array)
+        self.received_window[seq_num] = payload
+        self.process_window()
 
-        self.my_tunnel.magic_send(packet_byte_array)
+        ack = self.create_ack_packet(seq_num)
+        self.my_tunnel.magic_send(ack)
 
-    def get_seq_num(self, byte_array):
-        return struct.unpack("!H", byte_array[:2])[0]
-    
-    def get_ck_sum(self, byte_array):
-        return struct.unpack("!H", byte_array[-2:])[0]
+    def is_outside_window(self, seq_num):
+        distance = (seq_num - self.rcv_wnd_seq_num) & 0xFFFF # handles wrap around
+        return distance > self.window_size
+
+    def process_window(self):
+        # TODO: this assumes every packet must arrive, but this protocol supports % loss
+        while self.received_window[self.rcv_wnd_seq_num] is not None:
+            self.my_logger.commit(self.received_window[self.rcv_wnd_seq_num])
+            del self.received_window[self.rcv_wnd_seq_num]
+            self.rcv_wnd_seq_num = (self.rcv_wnd_seq_num + 1) & 0xFFFF
+
+    def create_ack_packet(self, ack_seq_num):
+        seq_bytes = struct.pack("!H", ack_seq_num)
+        bitmap = self.create_ack_bitmaps()
+        body = seq_bytes + bitmap
+
+        checksum = zlib.crc32(body) & 0xFFFF
+        ck_bytes = struct.pack("!H", checksum)
+        return body + ck_bytes
+
+
+    def create_ack_bitmaps(self):
+        bitmap = 0
+        for seq_num in self.received_window.keys():
+            bitmap |= (1 << (seq_num - self.rcv_wnd_seq_num)) # TODO: probably an issue here with wraparound
+
+        # Calculate number of bytes needed for window_size bits
+        num_bytes = math.ceil(self.window_size / 8) # Round up to nearest byte
+        return bitmap.to_bytes(num_bytes, byteorder='big')
+
 
     def run(self):
         ''' background loop as needed 
--- wildcat_sender.py
+++ wildcat_sender.py
@@ -4,7 +4,7 @@ import struct
 import zlib
 import random
 
-MAX_UINT16 = 65535
+
 
 class wildcat_sender(threading.Thread):
     def __init__(self, allowed_loss, window_size, my_tunnel, my_logger):
@@ -13,6 +13,7 @@ class wildcat_sender(threading.Thread):
 
         self.window_size = window_size
         self.inflight_window = {}
+        self.snd_wnd_seq_num = 0
 
         self.my_tunnel = my_tunnel
         self.my_logger = my_logger
@@ -20,65 +21,51 @@ class wildcat_sender(threading.Thread):
         # add as needed
         # start of cwnd (lowest#/oldest unACK'd pkt)
         self.base = 0
-        # seq num counter, start at 0 (end of cwnd, next seq num to send)
-        self.next_seq = 0
+
 
     def new_packet(self, packet_byte_array):
         ''' invoked when user sends a payload
         (Send with self.my_tunnel.magic_send(packet)) '''
         # build MSG: 2B seq (uint16), payload, 2B checksum (CRC32 & OxFFFF)
         # take curr seq (lowest 16 bits)
-        seq = self.next_seq & 0xFFFF
+        seq = self.snd_wnd_seq_num & 0xFFFF
         # pack seq num as 2Bs (big-endian)
         seq_bytes = struct.pack("!H", seq)
         # ensure payload is bytes
         payload_bytes = bytes(packet_byte_array)
         # body used for checksum = seq + payload
         body = seq_bytes + payload_bytes
-        # compute checksum & keep lower 16 bits
-        checksum = zlib.crc32(body) & 0xFFFF
+
+        checksum = compute_checksum(body)
         ck_bytes = struct.pack("!H", checksum)
+
         # final MSG = seq(2) + payload + checksum(2)
         msg = body + ck_bytes
-
-        print(f"sending : {packet_byte_array}")
-        # send thru magic tunnel
-        seq_num = self.get_and_inc_seq_num()
-        self.my_tunnel.magic_send(packet_byte_array)
         # adv seq num (wrap at 2^16)
-        self.next_seq = (self.next_seq + 1) & 0xFFFF
+        self.snd_wnd_seq_num = (self.snd_wnd_seq_num + 1) & 0xFFFF
 
-        timeout = threading.Timer(0.5, self.timeout_callback, args=(packet_byte_array,))
-        timeout.start()
-
-        self.inflight_window[seq_num] = (packet_byte_array, timeout)
+        self.send_packet(msg)
 
     def send_packet(self, byte_array_with_headers):
-        seq_num = self.get_seq_num(byte_array_with_headers)
+        print(f"sending : {byte_array_with_headers}")
+        seq_num = get_seq_num(byte_array_with_headers)
+
+        # actual send
         self.my_tunnel.magic_send(byte_array_with_headers)
 
         timeout = threading.Timer(0.5, self.timeout_callback, args=(byte_array_with_headers,))
         timeout.start()
         self.inflight_window[seq_num] = (byte_array_with_headers, timeout)
 
-
-    def get_seq_num(self, byte_array):
-        return struct.unpack("!H", byte_array[:2])[0]
-
-
     def timeout_callback(self, byte_array):
         print(f"timed out for : {byte_array}")
-        pass
-
-    def get_and_inc_seq_num(self):
-        self.current_seq_num = (self.current_seq_num + 1) % MAX_UINT16
-        return self.current_seq_num
+        # TODO: decide to resend based on target % loss
 
     def receive(self, packet_byte_array):
         ''' invoked when an ACK arrives '''
         # TODO: your implementation comes here
         print(f"received : {packet_byte_array}")
-        base = 
+        #base =
         pass
     
     def run(self):
@@ -90,4 +77,23 @@ class wildcat_sender(threading.Thread):
     
     def join(self):
         self.die = True
-        super().join(
+        super().join()
+
+def compute_checksum(byte_array) -> int:
+    # keep lower 16 bits to fit in 2 byte checksum header
+    return zlib.crc32(byte_array) & 0xFFFF
+
+def does_checksum_match(byte_array) -> bool:
+    return get_ck_sum(byte_array) == compute_checksum(get_seq_num_and_payload(byte_array))
+
+def get_seq_num(byte_array) -> int:
+    return struct.unpack("!H", byte_array[:2])[0]
+
+def get_ck_sum(byte_array) -> int:
+    return struct.unpack("!H", byte_array[-2:])[0]
+
+def get_payload(byte_array) -> bytes:
+    return byte_array[2:-2]
+
+def get_seq_num_and_payload(byte_array) -> bytes:
+    return byte_array[0:-2]
